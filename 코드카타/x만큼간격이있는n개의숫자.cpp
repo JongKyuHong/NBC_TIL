692972913bfc5c7f5b403b9e4efca2dba1c8143b
HongJongKyu <kongi7064@gmail.com>
2025-12-22 09:51:44 +0900

    add : 코드카타

---
+++ 코드카타/x만큼간격이있는n개의숫자.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int tmp = x;
+    while (answer.size()  < n){
+        answer.push_back(x);
+        x += tmp;
+    }
+    
+    return answer;
+}
