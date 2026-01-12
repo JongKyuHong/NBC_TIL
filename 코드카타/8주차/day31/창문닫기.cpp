0b62a576d5eb5464891de97b0d40d3d7c6a1e66d
HongJongKyu <kongi7064@gmail.com>
2026-01-12 21:42:04 +0900

    add : day31 코드카타 창문닫기

---
+++ 코드카타/8주차/day31/창문닫기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long N;
+    cin >> N;
+
+    cout << int(sqrt(N));
+
+
+    return 0;
+}
