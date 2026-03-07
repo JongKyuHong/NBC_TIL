06205901659c045d3429212157016b597cd81601
JongKyu <kongi7064@gmail.com>
2026-03-07 12:40:39 +0900

    add : 코드카타

---
+++ 코드카타/15주차/주말/알고리즘수업-깊이우선탐색1.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+int sequence = 1;
+
+void dfs(const int N, const int R, vector<vector<int>> &adj, vector<int> &visited)
+{
+    visited[R] = sequence++;
+    for (const int v : adj[R]){
+        if (!visited[v])
+        {
+            dfs(N, v, adj, visited);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, M, R;
+    cin >> N >> M >> R;
+    
+    vector<vector<int>> adj(N, vector<int>(0));
+    
+    for (int i = 0; i < M; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u-1].push_back(v-1);
+        adj[v-1].push_back(u-1);
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        if (adj[i].size() > 1)
+        {
+            sort(adj[i].begin(), adj[i].end());
+        }
+    }
+    vector<int> visited(N, 0);
+    dfs(N, R-1, adj, visited);
+    for (int i = 0; i < N; i++)
+    {
+        cout << visited[i] << "\n";
+    }
+    
+    return 0;
+}
