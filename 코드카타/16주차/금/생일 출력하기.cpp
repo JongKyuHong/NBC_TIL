9ab51b8f0f2ce7c490e92a492dd87aea14ec798c
JongKyu <kongi7064@gmail.com>
2026-03-13 08:58:36 +0900

    add : 코드카타

---
+++ 코드카타/16주차/금/생일 출력하기.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2555
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "3/19";    
+    
+    return 0;
+}
