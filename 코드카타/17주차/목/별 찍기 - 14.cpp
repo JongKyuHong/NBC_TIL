eefc36a206103d456117908d11d6bd074c65eeb5
JongKyu <kongi7064@gmail.com>
2026-03-19 09:59:26 +0900

    add : 코드카타

---
+++ 코드카타/17주차/목/별 찍기 - 14.cpp
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/2556
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
