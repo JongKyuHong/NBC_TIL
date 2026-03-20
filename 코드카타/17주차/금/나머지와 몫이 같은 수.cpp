e64456cc5c344142d9eadd68c2b3cd831c996d6e
JongKyu <kongi7064@gmail.com>
2026-03-20 10:23:50 +0900

    add : 코드카타, til[

---
+++ 코드카타/17주차/금/나머지와 몫이 같은 수.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    // N으로 나누었을때 나머지와 몫이 같은애들
+    
+    long long result = 0;
+    
+    for (int i = 1; i < N; i++)
+    {
+        // 여기까지
+        result += (long long)N*i + i;
+    }
+    
+    cout << result;
+    
+    return 0;
+}
