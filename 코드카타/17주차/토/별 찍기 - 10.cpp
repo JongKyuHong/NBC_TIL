05c3858a7d7b8e19d06db1f7592de3b6f5965e43
JongKyu <kongi7064@gmail.com>
2026-03-21 09:47:09 +0900

    add : 코드카타

---
+++ 코드카타/17주차/토/별 찍기 - 10.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void recursion(vector<vector<char>>& vec, int row, int col, int N)
+{
+    if (N == 3)
+    {
+        // cout << row << " " << col << "\n";
+        // 여기서 실제로 그림
+        for (int i = row; i < row+3; i++)
+        {
+            for (int j = col; j < col+3; j++)
+            {
+                if (i == row+1 && j == col+1) continue;
+                vec[i][j] = '*';
+            }
+        }
+        return;
+    }
+    
+    recursion(vec, row, col, N/3);
+    recursion(vec, row, col + N/3, N/3);
+    recursion(vec, row, col + N*2/3, N/3);
+    
+    recursion(vec, row + N/3, col, N/3);
+    // 중간 비우기
+    //recursion(vec, row*2/3, col*2/3, N/3);
+    recursion(vec, row + N/3, col + N*2/3, N/3);
+    
+    recursion(vec, row + N*2/3, col, N/3);
+    recursion(vec, row + N*2/3, col + N/3, N/3);
+    recursion(vec, row + N*2/3, col + N*2/3, N/3);
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    // 가운데 N/3 * N/3 정사각형
+    vector<vector<char>> vec(N, vector<char>(N, ' '));
+    // 항상기준은 왼쪽위
+    recursion(vec, 0, 0, N);
+    
+    for (const auto& row : vec)
+    {
+        for (const auto& col : row)
+        {
+            cout << col;
+        }
+        cout << "\n";
+    }
+    
+    
+    return 0;
+}
